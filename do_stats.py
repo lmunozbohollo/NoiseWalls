83bd93442b6314ad4995ac464de47804e722baa1
lmunozbohollo <lucia.munozbohollo@gmail.com>
2022-04-15 14:45:54 +0100

    Fixed error

--- do_stats.py
+++ do_stats.py
@@ -9,8 +9,7 @@ Created on Wed Mar 30 18:28:01 2022
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-import scipy.signal as signal
-import math
+from tabulate import tabulate
 
 from snr import findSNR
 from noisewall import calcNoiseWall
@@ -31,7 +30,10 @@ if __name__ == "__main__":
 SNRs = []
 NWs = []
 
-for participant in range(1,21):
+# partiicpants 2 and 6 not included due to issues with the recordings
+participants = ['1','3','4','5','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+
+for participant in participants:
     for task in TASKS:
         SNR = findSNR(participant,task)
         NW = calcNoiseWall(participant,task)
@@ -40,30 +42,45 @@ for participant in range(1,21):
         NWs.append(NW)
     
 SNRs = np.matrix(SNRs)
-SNRs = SNRs.reshape((10,20))
-SNRs = np.mean(SNRs, axis=1)
-
+SNRs = SNRs.reshape((18,10))
+SNRs_mean = np.mean(SNRs, axis=0)
+SNRs_var = np.var(SNRs, axis=0)
 
 NWs = np.matrix(NWs)
-NWs = NWs.reshape((10,20))
-NWs = np.mean(NWs, axis=1)
+NWs = NWs.reshape((18,10))
+NWs_mean = np.mean(NWs, axis=0)
+NWs_var = np.var(NWs, axis=0)
 
 
 
 a = []
-for x in np.array(SNRs).flat:
+for x in np.array(SNRs_mean).flat:
    a.append(x)
 
+a_var = []
+for x in np.array(SNRs_var).flat:
+   a_var.append(x)
+
 b = []
-for x in np.array(NWs).flat:
+for x in np.array(NWs_mean).flat:
    b.append(x)
 
+b_var = []
+for x in np.array(NWs_var).flat:
+   b_var.append(x)
 
 print(a)
 print(b)
 
 n_groups = len(TASKS)
 
+# print table with values and variances
+col_names = ['Task', 'Average SNR', 'Variance SNR', 'Average NW', 'Variance NW']
+data_points = [TASKS, a, a_var, b, b_var]
+points = np.array(data_points).T.tolist()
+
+# display values on table
+print(tabulate(points, headers = col_names, tablefmt = "fancygrid"))
 
 # create plot
 fig, ax = plt.subplots()
